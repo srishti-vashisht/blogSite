667b456c82dd485867fb7ac1ce0843b17b8f69f7
Srishti <srishti.vashisht@outlook.com>
2020-09-10 03:28:56 +0530

    Fourth

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");                                /*Require lodash library, giving constant name as _ when using lodash */
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -32,6 +33,39 @@ app.get("/about",function(req,res){
 app.get("/compose",function(req,res){
   res.render("compose");
 });
+
+/*Express routing parameters to tap into the url */
+
+ /* tapping into dynamic url , The name of route parameters must be made up of “word characters” ([A-Za-z0-9_]), don't use - or . also*/
+
+app.get("/posts/:anotherPost",function(req,res){                                  
+  //  console.log(req.params.anotherPost);   
+
+  //  for(var i=0 ; i<posts.length;i++){
+  //  if(req.params.anotherPost===posts[i].title){
+  //    console.log("Match Found");
+  //    res.redirect("/");
+
+  //  }
+   
+  // }
+
+  const requestedTitle = _.lowerCase(req.params.anotherPost);                               /*converting requestedTitle to lowercase using lodash */
+
+  posts.forEach(function(post){                                        /*Using for Each loop to loop into all title in posts arrray */
+    const storedTitle = _.lowerCase(post.title);                        /*converting storedTitle to lowercase using lodash , if Kebab case or whatsover format will get converted to lowercase*/
+
+         
+                             
+
+    if(requestedTitle===storedTitle)
+    {
+      console.log("Match Found");
+      res.redirect("/");
+    }
+  })
+   
+})
 
 app.post("/compose",function(req,res){
 
