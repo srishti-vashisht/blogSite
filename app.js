6cbbde2e0f1f262c9aacc9f651e8d2c67ee64bdd
Srishti <srishti.vashisht@outlook.com>
2020-09-09 23:56:27 +0530

    third

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-
+var posts=[];
 
 app.set('view engine', 'ejs');
 
@@ -17,7 +17,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/",function(req,res){
-  res.render("home",{homeContent: homeStartingContent});
+  res.render("home",{homeContent: homeStartingContent,postDetails:posts});
+                    
 })
 
 app.get("/contact",function(req,res){
@@ -42,7 +43,8 @@ app.post("/compose",function(req,res){
     content: req.body.blogContent
 
   }
-  res.redirect("compose");
+  posts.push(details);
+  res.redirect("/");
 })
 
 
